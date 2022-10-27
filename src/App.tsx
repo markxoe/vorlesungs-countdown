14a30764f18bfaff09b7ddd62e7045b1b78f5d04
markxoe <mark@oude-elberink.de>
2022-10-27 17:28:48 +0200

    feat: a little styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { useTime } from "./hooks/time";
 import { formatShit, getTimeSinceVorlesungsBeginn } from "./utils/time";
+import "./index.css";
 
 const App: FC = () => {
   const time = useTime(100);
 
   return (
-    <>
+    <div className="center">
       <h1>{formatShit(90 * 60 * 1000 - getTimeSinceVorlesungsBeginn(time))}</h1>
-    </>
+    </div>
   );
 };
 
