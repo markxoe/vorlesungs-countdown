0aaa29cf2e075f962f29e623e24bd750ed8a9797
markxoe <mark@oude-elberink.de>
2022-10-27 17:22:21 +0200

    chore: refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
 import { useTime } from "./hooks/time";
-import { formatShit, getVorlesungsbeginn } from "./utils/time";
+import { formatShit, getTimeSinceVorlesungsBeginn } from "./utils/time";
 
 const App: FC = () => {
   const time = useTime(100);
 
   return (
     <>
-      <h1>{formatShit(90 * 60 * 1000 - getVorlesungsbeginn(time))}</h1>
+      <h1>{formatShit(90 * 60 * 1000 - getTimeSinceVorlesungsBeginn(time))}</h1>
     </>
   );
 };
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,19 +1,20 @@
 import moment from "moment";
 
-const startOffset = 15 * 60 * 1000; // 15 minutes
+const startOffset = 15 * 60 * 1000; // 15 minutes (first Vorlesung would be at 00:15, not 00:00)
 const duration = 90 * 60 * 1000; // 90 minutes
+const timeBetweenVorlesungen = 2 * 60 * 60 * 1000; // 2 hours (firs Vorlesung 00:15, next 02:15)
 
 export const getTagesbeginn = (time: number) =>
   moment(time).startOf("day").valueOf();
 
-export const getVorlesungsbeginn = (time: number) => {
+export const getTimeSinceVorlesungsBeginn = (time: number) => {
   const timeOfDay = time - getTagesbeginn(time);
 
-  return (timeOfDay - startOffset) % (2 * 60 * 60 * 1000);
+  return (timeOfDay - startOffset) % timeBetweenVorlesungen;
 };
 
-export const getVorlesungsEnde = (time: number) =>
-  getVorlesungsbeginn(time) + duration;
+export const getTimeUntilVorlesungsEnde = (time: number) =>
+  getTimeSinceVorlesungsBeginn(time) + duration;
 
 export const formatShit = (milliseconds: number) => {
   const hours = Math.floor(milliseconds / 1000 / 60 / 60);
