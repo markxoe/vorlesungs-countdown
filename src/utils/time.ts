e13f9a0842caa0e31f4e1d8b02a588b4dea33be9
markxoe <mark@oude-elberink.de>
2022-10-27 22:08:12 +0200

    chore: renaming stuff

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,20 +1,20 @@
 import moment from "moment";
 
 const startOffset = 15 * 60 * 1000; // 15 minutes (first Vorlesung would be at 00:15, not 00:00)
-const duration = 90 * 60 * 1000; // 90 minutes
-const timeBetweenVorlesungen = 2 * 60 * 60 * 1000; // 2 hours (firs Vorlesung 00:15, next 02:15)
+const lectureDuration = 90 * 60 * 1000; // 90 minutes
+const timeBetweenLectures = 2 * 60 * 60 * 1000; // 2 hours (firs Vorlesung 00:15, next 02:15)
 
 export const getTagesbeginn = (time: number) =>
   moment(time).startOf("day").valueOf();
 
-export const getTimeSinceVorlesungsBeginn = (time: number) => {
+export const getTimeSinceLastLectureBegin = (time: number) => {
   const timeOfDay = time - getTagesbeginn(time);
 
-  return (timeOfDay - startOffset) % timeBetweenVorlesungen;
+  return (timeOfDay - startOffset) % timeBetweenLectures;
 };
 
-export const getTimeUntilVorlesungsEnde = (time: number) =>
-  getTimeSinceVorlesungsBeginn(time) + duration;
+export const getTimeUntilLectureEnd = (time: number) =>
+  getTimeSinceLastLectureBegin(time) + lectureDuration;
 
 export const formatShit = (milliseconds: number) => {
   const hours = Math.floor(milliseconds / 1000 / 60 / 60);
