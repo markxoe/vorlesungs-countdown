9d5397a7ebdcb9d72e4091280f0b8722a08c650b
markxoe <mark@oude-elberink.de>
2022-10-27 17:23:42 +0200

    fix: time formatting

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -21,5 +21,7 @@ export const formatShit = (milliseconds: number) => {
   const minutes = Math.floor(milliseconds / 1000 / 60 - hours * 60);
   const seconds = Math.floor(milliseconds / 1000 - minutes * 60 - hours * 3600);
 
-  return `${hours}:${minutes}:${seconds}`;
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
