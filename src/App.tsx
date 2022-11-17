84cc6318fa4d52a301bd133a414f8b2c9d6a81d2
markxoe <mark@oude-elberink.de>
2022-11-17 14:55:34 +0100

    feat: display percentage

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const confettiRunFor = 3 * 1000; // 5 seconds
 const App: FC = () => {
   const time = useTime(500);
   const [showSurvived, setShowSurvived] = useState(false);
+  const [showPercentage, setShowPercentage] = useState(false);
 
   const timeSinceLastLectureBegin = getTimeSinceLastLectureBegin(time);
 
@@ -37,28 +38,40 @@ const App: FC = () => {
             <h1>{formatShit(getNextLectureBegin(time) - time)}</h1>
           </>
         ) : (
-          <div onClick={() => setShowSurvived((i) => !i)}>
-            {showSurvived ? (
-              <>
-                <p>Vorlesung geht schon</p>
-                <h1>{formatShit(timeSinceLastLectureBegin)}</h1>
-              </>
-            ) : (
-              <>
-                <p>Vorlesung endet in</p>
-                <h1>
-                  {formatShit(lectureDuration - timeSinceLastLectureBegin)}
-                </h1>
-              </>
-            )}
+          <>
+            <div onClick={() => setShowSurvived((i) => !i)}>
+              {showSurvived ? (
+                <>
+                  <p>Vorlesung geht schon</p>
+                  <h1>{formatShit(timeSinceLastLectureBegin)}</h1>
+                </>
+              ) : (
+                <>
+                  <p>Vorlesung endet in</p>
+                  <h1>
+                    {formatShit(lectureDuration - timeSinceLastLectureBegin)}
+                  </h1>
+                </>
+              )}
+            </div>
             <progress
+              onClick={() => setShowPercentage((i) => !i)}
               value={
                 1 -
                 (lectureDuration - timeSinceLastLectureBegin) / lectureDuration
               }
               max={1}
             />
-          </div>
+            <p hidden={!showPercentage}>
+              {(
+                (1 -
+                  (lectureDuration - timeSinceLastLectureBegin) /
+                    lectureDuration) *
+                100
+              ).toFixed(2)}
+              %
+            </p>
+          </>
         )}
       </div>
     </div>
